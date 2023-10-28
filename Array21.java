6064d688f2aaf96722729b5062435f7b4506a50e
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-10-28 23:17:47 +0530

    Add files via upload

---
+++ Array21.java
@@ -0,0 +1,17 @@
+/**
+ * Array21
+ */
+public class Array21 {
+
+    public static void main(String[] args) {
+        // for(int i=0; i<4; i++){
+        //     System.out.println("****");
+        // }
+
+        int line=1;
+        while(line<=4){
+            System.out.println("****");
+            line++;
+        }
+    }
+}
