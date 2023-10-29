30b2def8b12c689e21c071cfcebbc1bc9cc7d19b
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-10-29 18:04:24 +0530

    Add files via upload

---
+++ array29.java
@@ -0,0 +1,11 @@
+public class array29 {
+    //countinue statement
+    public static void main(String[] args) {
+        for(int i=1; i<=13; i++){
+            if(i==5){
+                continue;
+            }
+            System.out.print(i);
+        }System.out.println();
+    }
+}
