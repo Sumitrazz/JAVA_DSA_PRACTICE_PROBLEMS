4474855c81ac5970f13a0690d2eb76cb8354ea08
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-10-30 20:46:11 +0530

    Add files via upload

---
+++ array37.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class array37 {
+    public static int calculateSum(int num1, int num2){
+        int sum=num1+ num2;
+        return sum;
+    }
+    public static void main(String[] args) {
+     Scanner sc =new Scanner(System.in);
+     int num1=sc.nextInt();
+     int num2=sc.nextInt();
+     System.out.println(calculateSum(num1, num2)); 
+    }
+}
