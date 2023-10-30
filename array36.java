6b3a653f65d771e80c582cbfa8283f9e3f92ff89
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-10-30 20:45:56 +0530

    Add files via upload

---
+++ array36.java
@@ -0,0 +1,8 @@
+public class array36 {
+    public static void printhelloworld(){
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        printhelloworld();
+    }
+}
