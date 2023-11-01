7ff17b4fb387af760591f9936cc49fce0a444ac0
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-01 15:05:01 +0530

    Add files via upload

---
+++ array41.java
@@ -0,0 +1,22 @@
+ /**
+  * array41
+  */
+ public class array41 {
+ public static int factorial(int n){
+    int f=1;
+    for(int i=1; i<=n; i++){
+        f=f*i;
+    }
+    return f;
+ }
+ public static int binomial(int n, int r){
+    int fact_n=factorial(n);
+    int fact_r=factorial(r);
+    int fact_nmr=factorial(n-r);
+    int binomial= fact_n/(fact_r*fact_nmr);
+    return binomial;
+ }
+    public static void main(String[] args) {
+        System.out.println(binomial(5, 2));
+    }
+ }
