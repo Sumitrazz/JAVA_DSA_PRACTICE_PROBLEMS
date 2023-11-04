5b91e95bb0b61140dc41a674daa9f38e61c39ca0
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-04 21:14:19 +0530

    Add files via upload

---
+++ array50.java
@@ -0,0 +1,22 @@
+/**
+ * array50
+ */
+public class array50 {
+
+    public static void hello_rectangle(int totRows, int totCols){
+        //outer loop
+        for(int i=1; i<=totRows; i++){
+            //inner loop
+            for(int j=1; j<=totCols; j++){
+                if(i==1 || i==totRows || j==1 || j==totCols){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        hello_rectangle(5, 4);
+    }
+}
