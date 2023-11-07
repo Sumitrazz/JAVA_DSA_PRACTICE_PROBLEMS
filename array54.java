4087580e86c94adf7fddbd71f68cd3808cd98e64
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-07 23:09:52 +0530

    Add files via upload

---
+++ array54.java
@@ -0,0 +1,16 @@
+public class array54 {
+    public static void zero_oneTringle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0 ");
+                }
+            }System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        zero_oneTringle(5);
+    }
+}
