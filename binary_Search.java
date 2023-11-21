f08f8520e0a32b33c26059c4dd520e5d9a371023
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-21 21:45:19 +0530

    Add files via upload

---
+++ binary_Search.java
@@ -0,0 +1,25 @@
+public class binary_Search{
+    public static int binarySearch(int number[],int key){
+int start=0;
+int end=number.length-1;
+// System.out.println(end);
+
+while(start<=end){
+    int mid=(start+end)/2;
+    if(number[mid]<key){
+        return mid;
+    } if(number[mid]<key){
+        start=mid+1;
+
+    } else{
+        end=mid-1;
+
+    }
+} return-1;
+
+    }public static void main(String args[]){
+        int number[]={2,4,6,8,10,12,14};
+        int key=10;
+        System.out.println("index for key is:="+binarySearch(number, key));
+    }
+}
