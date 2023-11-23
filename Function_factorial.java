0dfa79462afd345d0e5bf1ef21f08c50bcf3843b
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-24 00:30:53 +0530

    Add files via upload

---
+++ Function_factorial.java
@@ -0,0 +1,14 @@
+
+
+public class Function_factorial {
+    public static void factorial(int n){
+int f=1;
+for(int i=1; i<=n; i++){
+    f = f*i;
+System.out.println(f);
+} 
+
+    } public static void main(String args[]){
+factorial(5);
+    }
+}
