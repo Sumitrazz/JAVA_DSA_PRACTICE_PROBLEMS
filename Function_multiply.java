2eadac6be125032c101a47b5eb5c62e1f3646011
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-25 00:08:49 +0530

    Add files via upload

---
+++ Function_multiply.java
@@ -0,0 +1,9 @@
+public class Function_multiply {
+    public static void multiply(int a, int b){
+        int product = a*b;
+        System.out.println("multiply is ="+ product);
+    } public static void main(String args[]){
+        multiply(3,5);
+    }
+
+}
