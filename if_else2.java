34ea326725ef02a5518a4037655db436a68122d1
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-27 23:12:08 +0530

    Add files via upload

---
+++ if_else2.java
@@ -0,0 +1,13 @@
+public class if_else2 {
+    public static void main(String args[]){
+        int a= 1;
+        int b=5;
+        if(a>b){
+            System.out.println("A is largest of B");
+        } else {
+            System.out.println("B is Largest of A");
+            
+        }
+        
+    }
+}
