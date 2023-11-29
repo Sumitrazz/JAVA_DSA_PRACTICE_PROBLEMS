83d3cb84acf32668181766a8275349d7968184af
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-11-30 00:16:21 +0530

    Add files via upload

---
+++ Largest_of_N_if_else.java
@@ -0,0 +1,13 @@
+public class Largest_of_N_if_else {
+  public static void main(String args[]){
+    int a=1, b= 3, c =6;
+    if((a>=b)&& (a>=b)){
+        System.out.println("Largest is A");
+    } else if(b>=c){
+        System.out.println("Largest is B");
+
+    } else{
+        System.out.println("Largest is c");
+    }
+  }  
+}
