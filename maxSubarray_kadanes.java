b6e40d2d7ef6b4fa7cc09121599b336e29862b78
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-01 23:52:48 +0530

    Add files via upload

---
+++ maxSubarray_kadanes.java
@@ -0,0 +1,15 @@
+public class maxSubarray_kadanes {
+    public static void kadanes(int numbers[]){
+        int ms= Integer.MIN_VALUE;
+        int cs=0;
+        for(int i=0; i<numbers.length; i++){
+            cs += numbers[i];
+            if(cs<=0){
+                cs=0;
+            } ms= Math.max(cs,ms);
+        } System.out.println("our max subarray sum:"+ ms);
+    } public static void main(String args[]){
+        int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+        kadanes(numbers);
+    }
+}
