1541d04420ec9851893d88505be0998807081e67
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-02 00:08:29 +0530

    Add files via upload

---
+++ Operation1.java
@@ -0,0 +1,14 @@
+public class Operation1 {
+   public static void main(String args[]){
+int n= 5;
+//int b= n++;
+int c =++n;
+System.out.println(c);
+//System.out.println(b);
+
+
+    //for(int i=0; i<=10; i++){
+ //  System.out.println(i);
+// }    
+   } 
+}
