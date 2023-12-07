b93dc2ef9cc5458ce2427a86a9df0a2679e4ec58
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-07 23:13:30 +0530

    Add files via upload

---
+++ print_character_pattern.java
@@ -0,0 +1,12 @@
+public class print_character_pattern {
+    public static void main(String args[]){
+        int n=4;
+        char ch='A';
+        for(int line=1; line<=n; line++){
+            for(int a=1; a<=line; a++){
+                System.out.print(ch);
+                ch++;
+            } System.out.println();
+        }
+    }
+}
