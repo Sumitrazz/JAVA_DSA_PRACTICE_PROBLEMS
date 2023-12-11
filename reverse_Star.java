b619850e37fe94a1da8a6eabe59a2a3a356bbf5d
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-12 00:05:58 +0530

    Add files via upload

---
+++ reverse_Star.java
@@ -0,0 +1,11 @@
+public class reverse_Star {
+    public static void main(String args[]) {
+int n= 4; 
+for(int j=1; j<=n; j++){
+    for (int i=1; i<=n-j+1; i++){
+    System.out.print("*");
+}System.out.println();
+   } 
+
+}
+}
