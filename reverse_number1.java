d0b9294af6e6e31d198f6b801061b456830eb305
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-12 00:05:21 +0530

    Add files via upload

---
+++ reverse_number1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse_number1 {
+   
+    public static void main (String args[]){
+   Scanner sc = new Scanner(System.in);
+    int  n= sc.nextInt();
+   int  rev=0;
+    while(n>0){
+        int lastDigit= n%10;
+        rev = (rev*10)+ lastDigit;
+        n = n/10;
+
+    }
+System.out.println(rev);
+   } 
+}
