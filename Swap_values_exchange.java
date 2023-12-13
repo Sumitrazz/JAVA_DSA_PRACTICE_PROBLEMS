30cfdc5c76013365f5c7dc5e0b7654261232a7f6
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-13 20:50:26 +0530

    Add files via upload

---
+++ Swap_values_exchange.java
@@ -0,0 +1,12 @@
+public class Swap_values_exchange {
+   public static void main(String args[]){
+
+    int a= 5;
+    int b=10;
+    // swap
+    int temp= a;
+    a=b;
+    b=temp;
+     System.out.println("a="+a+"b="+b);
+    }
+}
