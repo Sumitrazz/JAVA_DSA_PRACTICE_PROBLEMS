c18aea6db2d0b518dcdd7e217011f9e1d910e6d7
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-15 00:10:27 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,22 @@
+public class Inheritance {
+    public static void main(String[] args) {
+      Fish shark=new Fish();
+      shark.eat();  
+    }
+}
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eats......");
+    }
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+// devived class
+class Fish extends Animal{
+    int fins;
+    void swim(){
+        System.out.print("Swims in water");
+    }
+}
