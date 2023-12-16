6a7205f04fcaae1da543da38313cee677589fb0d
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-16 22:54:32 +0530

    Add files via upload

---
+++ Recursion/find_the_first_occurence.java
@@ -0,0 +1,19 @@
+/**
+ * find_the_first_occurence
+ */
+public class find_the_first_occurence {
+public static int firstOcc(int arr[], int key, int i){
+    if(i==arr.length){
+        return -1;
+    }
+    
+    if(arr[i]==key){
+        return i;
+    }
+    return firstOcc(arr, key, i+1);
+}
+     public static void main(String[] args) {
+        int arr[]={2,3,4,5,6,7,8,9};
+        System.out.println(firstOcc(arr, 6, 0));
+     }
+}
