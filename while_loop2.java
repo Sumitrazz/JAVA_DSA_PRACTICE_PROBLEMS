6343b4791a19198f9d6893db21c60d50417b284d
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-20 21:36:04 +0530

    Add files via upload

---
+++ while_loop2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class while_loop2 {
+ public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int j=1;
+    while(j<=n){
+        System.out.println(j);
+        j++;
+    } 
+ }   
+}
