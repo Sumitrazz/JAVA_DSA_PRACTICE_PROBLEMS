0ebf75f3fa90cf0f3907a066010c869d528e8bf4
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-20 21:33:43 +0530

    Add files via upload

---
+++ while_loop5.java
@@ -0,0 +1,12 @@
+public class while_loop5 {
+     public static void main(String args[]){
+    //     for(int i=1; i<=4; i++){
+    //         System.out.println("*  *  *  *");
+    //     }
+    int n=1;
+    while(n<=4){
+        System.out.println("*  *  *  *");
+        n++;
+    }
+    }
+}
