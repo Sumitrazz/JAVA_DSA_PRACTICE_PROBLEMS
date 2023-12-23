4402b136d7d95341c3d16a6adc1e278574a040c6
SUMIT KUMAR <115313511+Sumitrazz@users.noreply.github.com>
2023-12-24 00:17:44 +0530

    Add files via upload

---
+++ Trapped_Rain_Water.java
@@ -0,0 +1,30 @@
+public class Trapped_Rain_Water{
+   public static int trappedRainWater(int height[]){
+      int n= height.length;
+      // left max value
+      int leftMax[]= new int [n];
+      leftMax[0]= height[0];
+      for(int i=1; i<n; i++){
+           leftMax[i]=Math.max(height[i], leftMax[i-1]);
+      } 
+      // right max value
+      int RightMax[]= new int [n];
+    RightMax[n-1]= height[n-1];
+    for(int i=n-2; i>=0; i--){
+      RightMax[i]= Math.max(height[i], RightMax[i+1]);
+
+    } 
+
+    int trappedWater=0;
+    for(int i=0; i<n; i++){
+      int waterLevel= Math.min(leftMax[i], RightMax[i]);
+      trappedWater +=waterLevel -height[i];
+    } return trappedWater;
+   }
+   public static void main(String[] args) {
+      int height[]={4,2,0,6,3,2,5};
+ System.out.println(trappedRainWater(height));
+   }
+   
+
+}
